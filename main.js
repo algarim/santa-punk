f0981b0f1090cc8208029f4f8e80f2777871f9e1
algarim <alvaro.garimberti@gmail.com>
2023-04-25 00:19:30 -0300

    JS: agrego arrays, objetos, y métodos de búsqueda y filtrado sobre arrays

--- main.js
+++ main.js
@@ -1,14 +1,51 @@
-// Defino variables con el precio de cada producto
+// Defino una clase que me permita crear un objeto para cada producto, con nombre, precio y cantidad de unidades del producto en el carrito (empieza siempre en 0)
+class Producto {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = Number(precio);
+        this.cantidadEnCarrito = 0;
+    }
+}
 
-let almohadon = 2600;
-let cuaderno = 2500;
-let combo = 7600;
+// Defino un objeto para cada producto
 
-// Defino variables que cuenten la cantidad de cada producto que se agregó al carrito
+const almohadon = new Producto("Almohadón Harry Potter", 2600);
+const cuaderno = new Producto("Cuaderno Totoro", 2500);
+const combo = new Producto("Combo Mario Bros", 7600);
+const almohadon2 = new Producto ("Almohadon Totoro", 2600)
 
-let almohadonCantidad = 0;
-let cuadernoCantidad = 0;
-let comboCantidad = 0;
+// Creo un array con todos los productos
+
+const listaDeProductos = [almohadon, cuaderno, combo, almohadon2];
+
+// Usando el método map, creo un array que contenga todos los enteros desde 1 hasta la longitud del array (en este caso: [1,2,3])
+
+indicesDeProductos = listaDeProductos.map( (producto) => listaDeProductos.indexOf(producto) + 1 );
+
+// Defino una función que liste en un string los elementos de un array de productos con sus precios (a modo de catálogo)
+
+function listarProductos(lista) {
+    let productosConPrecio = []; // array que va a ir agregando cada trozo de string, sobre el que después usaremos el método join
+    for (let i = 0; i < lista.length; i++) {
+        productosConPrecio.push(" " + (i + 1) + ". " + listaDeProductos[i].nombre + " - $ " + listaDeProductos[i].precio);
+    }
+    return productosConPrecio.join("\n");
+}
+
+// Defino una función que liste en un string los productos agregados al carrito con sus respectivas cantidades
+
+function verCarrito(lista) {
+    let carrito = "";
+    for (let i = 0; i < lista.length; i++) {
+        if (lista[i].cantidadEnCarrito > 0) {
+            carrito = carrito + "\n" + lista[i].nombre + " x " + lista[i].cantidadEnCarrito;
+        }
+    }
+    if (carrito === "") {
+        carrito = "Tu carrito está vacío";
+    }
+    return carrito;
+}
 
 // Defino una variable que lleve el precio total acumulado
 
@@ -29,44 +66,30 @@ let seleccionCarrito;
 // Defino el ciclo por el cual el usuario agrega productos al carrito
 
 while (seleccionCarrito != 3) {
-    seleccionBienvenida = Number(prompt("¡Bienvenido a la tienda de Santa Punk! \n\nElegí un producto para agregar al carrito: \n\n 1. Almohadón Harry Potter - $ 2600 \n 2. Cuaderno Totoro - $ 2500 \n 3. Combo Mario Bros - $ 7600 \n\nTambién podés acceder al carrito para revisar los productos elegidos y ver el precio final: \n\n4. Ver carrito"));
+    seleccionBienvenida = Number(prompt("¡Bienvenido a la tienda de Santa Punk! \n\nElegí un producto para agregar al carrito: \n\n" + listarProductos(listaDeProductos) + "\n\nTambién podés acceder al carrito para revisar los productos elegidos y ver el precio final: \n\n" + (listaDeProductos.length + 1) + ". Ver carrito"));
 
-    switch (seleccionBienvenida) {
-
-        case 1:
-            sumarPrecio(almohadon);
-            almohadonCantidad = almohadonCantidad + 1;
+    for (let i = 0; i < listaDeProductos.length; i++) {
+        if (seleccionBienvenida == (i + 1) ) {
+            sumarPrecio(listaDeProductos[i].precio);
+            listaDeProductos[i].cantidadEnCarrito = listaDeProductos[i].cantidadEnCarrito + 1;
             break;
+        }
+    }
 
-        case 2:
-            sumarPrecio(cuaderno);
-            cuadernoCantidad = cuadernoCantidad + 1;
-            break;
+    if (seleccionBienvenida == (listaDeProductos.length + 1) ) {
+        seleccionCarrito = Number(prompt(verCarrito(listaDeProductos) + "\n     Precio total: $ " + precioTotal + "\n\n 1. Elegir otro producto \n 2. Vaciar carrito \n 3. Finalizar compra"))
 
-        case 3:
-            sumarPrecio(combo);
-            comboCantidad = comboCantidad + 1;
-            break;
-
-        case 4:
-            seleccionCarrito = Number(prompt("Tu carrito: \n\n Almohadon Harry Potter - " + almohadonCantidad + " unidad/es \n Cuaderno Totoro - " + cuadernoCantidad + " unidad/es \n Combo Mario Bros - " + comboCantidad + " unidad/es \n\n Precio total: $ " + precioTotal + "\n\n 1. Elegir otro producto \n 2. Vaciar carrito \n 3. Finalizar compra"))
-
-            if (seleccionCarrito == 2) {
-                precioTotal = 0;
-                almohadonCantidad = 0;
-                cuadernoCantidad = 0;
-                comboCantidad = 0;
-                alert("Se vació el carrito")
-            }
-            else if ( (seleccionCarrito != 1) && (seleccionCarrito != 3) ) {
-                alert("Comando no reconocido. Por favor, ingrese el número correspondiente a la opción elegida.")
-            }
-
-            break;
-
-        default:
-            alert("Comando no reconocido. Por favor, ingrese el número correspondiente a la opción elegida.");
-            break;
+        if (seleccionCarrito == 2) {
+            precioTotal = 0;
+            listaDeProductos.forEach( (producto) => producto.cantidadEnCarrito = 0 );
+            alert("Se vació el carrito")
+        }
+        else if ((seleccionCarrito != 1) && (seleccionCarrito != 3)) {
+            alert("Comando no reconocido. Por favor, ingresá el número correspondiente a la opción elegida.")
+        }
+    }
+    else if ( !indicesDeProductos.includes(seleccionBienvenida)) {
+        alert("Comando no reconocido. Por favor, ingresá el número correspondiente a la opción elegida.");
     }
 
 }
